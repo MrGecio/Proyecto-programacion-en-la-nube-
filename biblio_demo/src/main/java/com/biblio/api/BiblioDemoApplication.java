f09a2fa2af0caf588aad169b69fa168bcf440fff
Gecio <geciobri@gmail.com>
2025-11-13 16:50:13 -0600

    Base de lo que viene en el examen todo esta en el demo

---
+++ biblio_demo/src/main/java/com/biblio/api/BiblioDemoApplication.java
@@ -0,0 +1,13 @@
+package com.biblio.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblioDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblioDemoApplication.class, args);
+	}
+
+}
---
+++ biblio_demo/src/test/java/com/biblio/api/BiblioDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblio.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblioDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
